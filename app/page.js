61658b183c5b7d3fb26b43556544ca1c6e48cfc2
mugabilenny-cloud <mugabilenny@gmail.com>
2026-08-22 10:13:53 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Campus Notes Repository</h1>
+      <p>Database connected. Platform ready to host all 70 courses.</p>
+    </main>
+  );
+}
